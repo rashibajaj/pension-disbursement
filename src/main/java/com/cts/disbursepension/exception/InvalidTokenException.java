d9bcc323d3a0222854cae180626bd65204b10fa1
Bajaj <909121@cognizant.com>
2022-08-23 16:55:17 +0530

    pensiondisb

---
+++ src/main/java/com/cts/disbursepension/exception/InvalidTokenException.java
@@ -0,0 +1,10 @@
+package com.cts.disbursepension.exception;
+
+public class InvalidTokenException extends Exception {
+	private static final long serialVersionUID = -5091342239524021914L;
+
+	public InvalidTokenException(String message) {
+		super(message);
+	}
+
+}
